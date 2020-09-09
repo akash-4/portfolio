061d3da13b1d70b2bbc423b356c4cc0ab7d3d958
Akash Agarwal <akashagarwal41200@gmail.com>
2020-09-10 03:08:37 +0530

    Final Changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,19 +29,19 @@ function App() {
             <h1 class="Heading">Projects</h1>
           </div>
           {showMore == true ? <div className="container projects">
-            <Projects title="FilmGenie" img="images/a.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
-            <Projects title="PokeDex" img="images/a.png" desc="An App for the Pokemon fans." tech="Flutter | Firebase" />
-            <Projects title="ProgCroc" img="images/a.png" desc="Educational Website providing resources on C++ for class 11th and 12th students." tech="HTML | CSS | JS" />
+            <Projects title="FilmGenie" img="images/filmgenie.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
+            <Projects title="PokeDex" img="images/pokedex.png" desc="An App for the Pokemon fans." tech="Flutter | Firebase" />
+            <Projects title="ProgCroc" img="images/prog.jpg" desc="Educational Website providing resources on C++ for class 11th and 12th students." tech="HTML | CSS | JS" />
             {/* <Projects title="Bottle App" img="images/a.png" desc={"Its a Productivity tool. A project I am currently working on for Bottle Co."} tech="Flutter | Firebase" />
            <Projects title="Bottle Backend" img="images/a.png" desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit." tech="REST API" /> */}
             <Projects title="NoteKeeper" img="images/a.png" desc="Simple WebApp for maintaing notes." tech="MongoDB | Express | React | NodeJS" />
-            <Projects title="Dice Game" img="images/a.png" desc="A minimilistic Game designed for all kinds of people." tech="HTML | CSS | JS" />
-            <Projects title="Simon Game" img="images/a.png" desc="The Famous Simon Game. Test your memory through this game. " tech="HTML | CSS | JS" />
+            <Projects title="Dice Game" img="images/dice.png" desc="A minimilistic Game designed for all kinds of people to decide winner between the two by throwing a dice." tech="HTML | CSS | JS" />
+            <Projects title="Simon Game" img="images/simon.png" desc="The Famous Simon Game. Test your memory through this game. " tech="HTML | CSS | JS" />
 
           </div> : <div className="container projects">
-              <Projects title="FilmGenie" img="images/a.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
-              <Projects title="PokeDex" img="images/a.png" desc="An App for the Pokemon fans." tech="Flutter | Firebase" />
-              <Projects title="ProgCroc" img="images/a.png" desc="Educational Website providing resources on C++ for class 11th and 12th students." tech="HTML | CSS | JS" />
+              <Projects title="FilmGenie" img="images/filmgenie.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
+              <Projects title="PokeDex" img="images/pokedex.png" desc="An App for the Pokemon fans." tech="Flutter | Firebase" />
+              <Projects title="ProgCroc" img="images/prog.jpg" desc="Educational Website providing resources on C++ for class 11th and 12th students." tech="HTML | CSS | JS" />
               <Projects title="NoteKeeper" img="images/a.png" desc="Simple WebApp for maintaing notes." tech="MongoDB | Express | React | NodeJS" />
 
             </div>}
