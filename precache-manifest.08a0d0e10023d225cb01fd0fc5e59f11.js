8d2f301a9ee1710008312f2f0b79d89f8f10f6ae
Akash Agarwal <akashagarwal41200@gmail.com>
2020-09-09 23:44:20 +0530

    Updates

---
+++ precache-manifest.08a0d0e10023d225cb01fd0fc5e59f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3cd4206a7d135e8bcabe121f2eabc1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b6c30b9a93a2e307c90",
+    "url": "/portfolio/static/js/2.9033755a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.9033755a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a6646f1251ca9e6367",
+    "url": "/portfolio/static/js/main.1094e621.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
