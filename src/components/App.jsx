b614d82ac39c5dd6e815a65f0a0f5403b55299c4
Akash Agarwal <akashagarwal41200@gmail.com>
2020-11-26 03:36:01 +0530

    Latest Changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,13 +31,14 @@ function App() {
           {showMore == true ? <div className="container projects">
             <Projects title="FilmGenie" img="images/filmgenie.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
             <Projects title="PokeDex" img="images/pokedex.png" desc="An App for the Pokemon fans." tech="Flutter | Firebase" />
-            <Projects title="Netflix Clone" img="images/netflix.jpg" desc="Netflix Clone " tech="React" />
-            <Projects title="Spotify Clone" img="images/spotify.png" desc="Spotify Clone with login functionalitya and fetches user playlists " tech="React" />
+            <Projects title="Netflix Clone" img="images/netflix.jpg" desc="Simplistic Netflix Clone " tech="React" />
+            <Projects title="Spotify Clone" img="images/spotify.png" desc="Spotify Clone with login functionality which user playlists " tech="React" />
            <Projects title="Bottle App" img="images/bottle.png" desc="Its a Productivity tool. A project I am currently working on for Bottle Co." tech="Flutter | Firebase" />
-           <Projects title="Bottle Backend" img="images/bottle.png" desc="Highly Scalable Backend designed for Bottle." tech="Node | Express | AWS | Sockets" /> 
+           <Projects title="Bottle Backend" img="images/bottle.png" desc="Highly Scalable Backend designed for Bottle." tech="Mongo | Node | Express | AWS | Sockets" /> 
             <Projects title="NoteKeeper" img="images/a.png" desc="Simple WebApp for maintaing notes." tech="MongoDB | Express | React | NodeJS" />
             <Projects title="Dice Game" img="images/dice.png" desc="A minimilistic Game designed for all kinds of people to decide winner between the two by throwing a dice." tech="HTML | CSS | JS" />
             <Projects title="Simon Game" img="images/simon.png" desc="The Famous Simon Game. Test your memory through this game. " tech="HTML | CSS | JS" />
+            <Projects title="Warehouse Management App" img="images/jd.jpg" desc="A Warehouse Management App build for JD Bazaar which offers complete solution to all warehouse management related things. " tech="Flutter | Firebase" />
 
           </div> : <div className="container projects">
           <Projects title="FilmGenie" img="images/filmgenie.png" desc="The most happening movie community for movie geeks." tech="Flutter | Firebase" />
